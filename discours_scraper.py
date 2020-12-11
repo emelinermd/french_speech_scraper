45b6a34075a66cc650a25cb5bc245fbd8643916c
Araxide <emelineraimond@gmail.com>
2020-12-11 08:35:45 +0100

    feat : scrap from begin_page

--- discours_scraper.py
+++ discours_scraper.py
@@ -9,7 +9,8 @@ class DiscoursScraper():
     self.driver = webdriver.Chrome(ChromeDriverManager().install())
     self.main_page_url = 'https://www.vie-publique.fr/discours'
     self.child_page_url = 'https://www.vie-publique.fr/discours?page='
-    self.pages_total = 10;
+    self.pages_begin = 1;
+    self.pages_end = 100;
     
   def scrap_speeches_urls(self, page_url):
     self.driver.get(page_url)
@@ -39,7 +40,7 @@ class DiscoursScraper():
 
   def main(self):
     # create csv
-    with open('discours.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    with open('discours_2020_3.csv', 'a', encoding='utf-8', newline='') as csvfile:
       fieldnames = ['titre', 'date', 'discours']
       writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
       writer.writeheader()
@@ -47,7 +48,7 @@ class DiscoursScraper():
       speeches_urls_first_page = self.scrap_speeches_urls(self.main_page_url)
       self.record_speeches_of_page(writer, speeches_urls_first_page)      
       # get speeches urls for each next page until reach pages_total
-      for i in range(1, self.pages_total):
+      for i in range(self.pages_begin, self.pages_end):
         speeches_urls = self.scrap_speeches_urls(self.child_page_url+str(i))
         # scrap speech text
         self.record_speeches_of_page(writer, speeches_urls)
